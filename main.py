a53862477aa73585688b1b853f6bb57b390578fd
PressXFor <dkartsen1@gmail.com>
2022-03-22 11:29:01 -0400

    I went about this the wrong way. I need to make an attractor for the particles, and have the mouse act as a collider. I tried making the mouse a reflector, then the particles move to their original position.
    Will update with working code.

--- main.py
+++ main.py
@@ -5,33 +5,31 @@ pygame.init()
 
 
 class Particle:
-    def __init__(self, color, position, screen):
+    def __init__(self, color, position, screen, startDir):
         self.color = color
-        self.pos = position
+        self.pos = pygame.math.Vector2(position)
+        self.dir = pygame.math.Vector2(startDir).normalize()
         self.screen = screen
         self.size = (1, 1)
-        self.image = pygame.Surface(self.size)
-        self.image.fill(self.color)
-        self.rect = self.image.get_rect(topleft=self.pos)
 
-    def move(self):
-        self.pos = (self.pos[0] + random.uniform(-1, 1), self.pos[1] + random.uniform(-1, 1))
-        self.rect.topleft = self.pos
+    # def move(self):
+    #     self.pos = (self.pos[0] + random.uniform(-1, 1), self.pos[1] + random.uniform(-1, 1))
 
     def draw(self):
-        self.screen.blit(self.image, self.rect)
+        pygame.draw.rect(self.screen, self.color, pygame.Rect(self.pos, self.size))
+
+    # def reflect(self, newDir):
+    #     self.dir = self.dir.reflect(pygame.math.Vector2(newDir))
 
 
 screen = pygame.display.set_mode((700, 500))
 clock = pygame.time.Clock()
 
-font = pygame.font.SysFont('arial', 32)
-image = font.render(input("Enter your text\n"), True, (255, 0, 0))
-# image = pygame.image.load("testimage.png").convert_alpha()
+image = pygame.image.load("testimage.png").convert_alpha()
 image_rect = image.get_rect(topleft=(100, 100))
 
 
-def generate_particles(screen, relative_position, image):
+def generate_particles(screen, relative_position, image, startDir):
     particles = []
     alphas = pygame.surfarray.array_alpha(image)
     colors = pygame.surfarray.array3d(image)
@@ -40,13 +38,18 @@ def generate_particles(screen, relative_position, image):
             if alpha:
                 pos = (relative_position[0] + i, relative_position[1] + j)
                 color = colors[i][j]
-                new_particle = Particle(color, pos, screen)
+                new_particle = Particle(color, pos, screen, startDir)
                 particles.append(new_particle)
     return particles
 
 
-particles = generate_particles(screen, (100, 100), image)
-particle_rects = [p.rect for p in particles]
+particles = generate_particles(screen, (100, 100), image, startDir=(0, 1))
+
+# d = sqrt((x2 - x1)^2 + (y2 - y1)^2)
+# def collision():
+#     if generate_particles() <= mouse_pos:
+#         print('hellos')
+
 
 running = True
 while running:
@@ -55,10 +58,20 @@ while running:
             running = False
 
     screen.fill((255, 255, 255, 255))
+
+    mouse_pos = pygame.mouse.get_pos()
+    cursor = pygame.draw.circle(screen, 'Red', mouse_pos, 20)
+
+    # for new_particle in particles:
+    #     d = new_particle.pos.distance_to(mouse_pos)
+    #     if d <= 20:
+            # new_particle.pos[0]
+            # new_particle.pos[1]
+
+    # collision()
     for particle in particles:
         particle.draw()
         # particle.move()
-    pygame.display.update()
+    pygame.display.flip()
 
-    print(clock.get_fps())
     clock.tick(60)
